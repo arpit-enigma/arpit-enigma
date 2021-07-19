d76ca463be9211325956ff1904bc62a0b64e74e6
arpit-enigma <45957839+arpit-enigma@users.noreply.github.com>
2021-07-19 13:45:03 +0530

    na

---
+++ src/audi/AudiClass.java
@@ -0,0 +1,9 @@
+package audi;
+
+public class AudiClass {
+
+	public static void main(String[] args) {
+System.out.println("Arpit's audi");
+	}
+
+}
---
+++ src/bmw/BMWClass.java
@@ -0,0 +1,9 @@
+package bmw;
+
+public class BMWClass {
+
+	public static void main(String[] args) {
+System.out.println("Arpit's BMW");
+	}
+
+}
